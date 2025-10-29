8bf66eedf112d177eb3fee97e776662806cc1332
yueyueyan <yueyueyan1215@163.com>
2025-10-29 16:24:47 +0800

    modified:   .vitepress/sideBarConfig.ts

--- .vitepress/sideBarConfig.ts
+++ .vitepress/sideBarConfig.ts
@@ -40,6 +40,18 @@ export const sideBarConfig: DefaultTheme.Sidebar = {
           text: "Promise 和 async 函数",
           link: "/es6/003",
         },
+        {
+          text: "变量类型和变量计算",
+          link: "/es6/004",
+        },
+        {
+          text: "原型链",
+          link: "/es6/005",
+        },
+        {
+          text: "作用域和闭包",
+          link: "/es6/006",
+        },
       ],
     },
   ],
